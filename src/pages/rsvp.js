182e2119e4c8a860d9eecd840123d617004c3a79
alexanderjones19 <alexanderjones4215@gmail.com>
2019-08-30 15:17:37 -0400

    rsvp form added, navbar stylings

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import Head from '../components/head';
+import RsvpStyles from './rsvp.module.scss';
 
 const RsvpPage = () => {
   return (
     <Layout topMargin>
       <Head title="RSVP" />
-      <h1>RSVP</h1>
+      <section className="section">
+        <div className="container">
+          <p className={RsvpStyles.content}>Feel free to RSVP utilizing the form below, or through the traditional method.</p>
+        </div>
+      </section>
+      <section className="section">
+        <div className={`container ${RsvpStyles.formContainer}`}>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdAH2PxLq6opruisO7mESzxfJN9WmZCBk6nBWAGnVto7s29CQ/viewform?embedded=true" width="640" height="808" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+        </div>
+      </section>
     </Layout>
   );
 }
