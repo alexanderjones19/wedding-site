d6f4f4b392d31156376674e5857ccabd5befe6a6
alexanderjones19 <alexanderjones4215@gmail.com>
2019-09-04 10:31:18 -0400

    rsvp stylings

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -2,15 +2,29 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import Head from '../components/head';
-import RsvpStyles from './rsvp.module.scss';
+import rsvpStyles from './rsvp.module.scss';
 
 const RsvpPage = () => {
   return (
     <Layout topMargin>
       <Head title="RSVP" />
-      <section className="section">
-        <div className={`container ${RsvpStyles.formContainer}`}>
+      <section className={`section ${rsvpStyles.sectionStyles}`}>
+        <div className="container">
+          <h2 className={`title is-2 ${rsvpStyles.textContent}`}>
+            RSVP
+          </h2>
+        </div>
+      </section>
+      <section className={`section ${rsvpStyles.sectionStyles}`}>
+        <div className={`container ${rsvpStyles.formContainer}`}>
           <iframe title="RSVP Form" src="https://docs.google.com/forms/d/e/1FAIpQLSdAH2PxLq6opruisO7mESzxfJN9WmZCBk6nBWAGnVto7s29CQ/viewform?embedded=true" width="640" height="808" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <h4 className={`subtitle is-4 ${rsvpStyles.textContent}`}>
+            Email us: edmondsjoneswedding@gmail.com
+          </h4>
         </div>
       </section>
     </Layout>
