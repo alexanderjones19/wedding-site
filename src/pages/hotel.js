d9253795c5599df605308f5e935bd07a2c7f8352
alexanderjones19 <alexanderjones4215@gmail.com>
2019-11-07 21:04:23 -0500

    update hotel page

--- src/pages/hotel.js
+++ src/pages/hotel.js
@@ -9,12 +9,17 @@ const HotelPage = () => {
     <Layout topMargin>
       <Head title="Hotel" />
       <section className="section">
-        <div className="container">
+        <div className={`container ${hotelStyles.container}`}>
           <h2 className={`title is-2 is-size-4-mobile ${hotelStyles.textContent}`}>
             Hotel
           </h2>
-          <h4 className={`subtitle is-4 is-size-6-mobile ${hotelStyles.textContent}`}>
-            Information Coming Soon
+          <a className={hotelStyles.anchor} href="https://www.airbnb.com/s/Virginia-Beach--VA--United-States/homes" target="_blank" rel="noopener noreferrer">
+            <h4 className={`subtitle is-4 is-size-6-mobile ${hotelStyles.textContent}`}>
+              Airbnb
+            </h4>
+          </a>
+          <h4 className={`subtitle is-4 is-size-6-mobile ${hotelStyles.textContent} ${hotelStyles.marginTop}`}>
+             More Hotel Information Coming Soon
           </h4>
         </div>
       </section>
