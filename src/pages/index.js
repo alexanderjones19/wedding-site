5b14b2261d6b327e7443872ca0a594e2f2175e88
alexanderjones19 <alexanderjones4215@gmail.com>
2020-03-22 16:42:03 -0400

    updated index.js message

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,11 +47,23 @@ const IndexPage = ({ data }) => {
 
       <section className="section">
         <div className="container">
-          <h2 className={`title is-2 is-size-4-mobile ${indexStyles.textContent}`}>
+          {/* <h2 className={`title is-2 is-size-4-mobile ${indexStyles.textContent}`}>
             Hello Friends and Family!
           </h2>
           <h4 className={`subtitle is-4 is-size-6-mobile ${indexStyles.textContent}`}>
             Thank you so much for being a part of our special day! Here you will find all of the information you need in preparation for our wedding. Please let us know if you have any lingering questions, you can find our email on the RSVP page. We love all of you, can't wait until April!
+          </h4>
+          <h5 className={`subtitle is-5 is-size-7-mobile ${indexStyles.textContent}`}>
+            - Constance + Alexander
+          </h5> */}
+          <h2 className={`title is-2 is-size-4-mobile ${indexStyles.textContent}`}>
+            Important Update:
+          </h2>
+          <h4 className={`subtitle is-4 is-size-6-mobile ${indexStyles.textContent}`}>
+            In light of the coronavirus pandemic, and due to closures we couldn't control, we unfortunately have made the decision to postpone our wedding until July 17th, 2020. We hope that everyone will still be able to join us! In the meantime, please let us know if you have any questions or concerns.
+          </h4>
+          <h4 className={`subtitle is-4 is-size-6-mobile ${indexStyles.textContent}`}>
+            edmondsjoneswedding@gmail.com
           </h4>
           <h5 className={`subtitle is-5 is-size-7-mobile ${indexStyles.textContent}`}>
             - Constance + Alexander
