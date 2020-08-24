70b0e168e441d2e1f2b22fe3b285412f9c8d094a
alexanderjones19 <alexanderjones4215@gmail.com>
2020-08-23 23:39:36 -0400

    update index page with link to photo gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,37 +48,17 @@ const IndexPage = ({ data }) => {
 
       <section className="section">
         <div className={`container ${indexStyles.container}`}>
-          {/* <h2 className={`title is-2 is-size-4-mobile ${indexStyles.textContent}`}>
-            Hello Friends and Family!
-          </h2>
-          <h4 className={`subtitle is-4 is-size-6-mobile ${indexStyles.textContent}`}>
-            Thank you so much for being a part of our special day! Here you will find all of the information you need in preparation for our wedding. Please let us know if you have any lingering questions, you can find our email on the RSVP page. We love all of you, can't wait until April!
-          </h4>
-          <h5 className={`subtitle is-5 is-size-7-mobile ${indexStyles.textContent}`}>
-            - Constance + Alexander
-          </h5> */}
-          <a className={indexStyles.link} href="https://us04web.zoom.us/j/71392285592?pwd=V0gzTCtybjBKcUt6N0hPWmlqeXI2dz09" target="_blank" rel="noopener noreferrer">
+          <a className={indexStyles.link} href="https://rebeccaburtphotography.pic-time.com/-constancealexander/login?ptat=AAAAAFkAAAAMBn3zdfPRETQi4WCfOVE7BB5xoAVxUKlHykQq13AXmqCUwQWCqXnORfFua6pQZx7PaD2ynmGYI96dP7XCBx5z&redirect_back=%2f-constancealexander%2fgallery%3fptat%3dAAAAAFkAAAAMBn3zdfPRETQi4WCfOVE7BB5xoAVxUKlHykQq13AXmqCUwQWCqXnORfFua6pQZx7PaD2ynmGYI96dP7XCBx5z" target="_blank" rel="noopener noreferrer">
             <h2 className={`title is-2 is-size-4-mobile ${indexStyles.textContent}`}>
-              Click here to view Ceremony Zoom stream
+              Click here to view wedding photo gallery
             </h2>
           </a>
-          <h4 className={`subtitle is-4 is-size-6-mobile ${indexStyles.textContent}`}>
-            Zoom meeting passcode: 9zPb9r
-          </h4>
           <h4 className={`subtitle is-4 is-size-6-mobile ${indexStyles.textContent}`}>
             Thank you so much for being a part of our special day!
           </h4>
           <h5 className={`subtitle is-5 is-size-7-mobile ${indexStyles.textContent}`}>
             - Constance + Alexander
           </h5>
-          <Link
-            to='/schedule/'
-            className={indexStyles.link}
-          >
-            <h2 className={`title is-2 is-size-4-mobile ${indexStyles.textContent}`}>
-              Click here to view Schedule + COVID-19 guidelines  
-            </h2>
-          </Link>
         </div>
       </section>
       
